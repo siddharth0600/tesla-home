bfae5a1ba71f732c6e29475f7d7cb438bcf340cd
siddharth0600 <siddharthtripathi00@gmail.com>
2021-03-16 15:12:22 +0530

    Header + assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Tesla Homew page</h1>
+      <Header/>
+      {/* Items => Snapping */}
+      {/* Items => Snapping */}
+      {/* Items => Snapping */}
+      {/* Items => Snapping */}
     </div>
   );
 }
